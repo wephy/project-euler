ee5aa29bff71aab90bfef76e8abfb5664bb4a795
Joseph <xexpozure@gmail.com>
2021-06-07 19:22:45 +0100

    p066

--- python/p066.py
+++ python/p066.py
@@ -1,48 +1,36 @@
 # Diophantine equation
 
-
 import numpy as np
 from fractions import Fraction
-import time
 
 
-def minimal_solution(n):
-    print("\n\n", n)
-
-    if np.sqrt(n) % 1 == 0:
+def minimal_solution(s):
+    if np.sqrt(s) % 1 == 0:
         return 0
 
-    a = np.ceil(np.sqrt(n))
-    b = 1
-    k = a ** 2 - n * b ** 2
-    print(a, b, k)
+    a0 = int(np.floor(np.sqrt(s)))
+    a = a0
+    m = 0
+    d = 1
 
-    m0 = np.sqrt(n)
-    while k != 1:
-        ms = []
-        for i in range(0, int(m0 + 2)):
-            if (a + b * i) / k % 1 == 0:
-                ms += [i]
-        print(ms)
-        m = min(ms, key=lambda x: abs(n - x ** 2))
-        print("m: ", m)
-        a, b, k = (m * a + n * b) // abs(k), (a + b * m) // abs(k), (m ** 2 - n) // k
-        # bp = (a + b * m) / abs(k)
-        # kp = (m ** 2 - n) / k
+    answer = Fraction(0, 1)
+    denominators = []
+    while answer.numerator ** 2 - s * answer.denominator ** 2 != 1:
+        m = (d * a) - m
+        d = (s - (m ** 2)) / d
+        a = int(np.floor((a0 + m) / d))
+        denominators += [a]
 
-        print(a, b, k)
-        # time.sleep(1)
+        answer = 0
+        for den in denominators[::-1]:
+            answer = Fraction(1, den + answer)
+        answer += a0
 
-    return int(a)
+    return answer.numerator
 
-minimal_solution(181)
-# print(2 ** 52)
 
-# solutions = {}
-# for i in range(2, 1000 + 1):
-#     solutions[i] = minimal_solution(i)
+solutions = {}
+for i in range(2, 1000 + 1):
+    solutions[i] = minimal_solution(i)
 
-# print(max(solutions, key=solutions.get))
-# 183567298683461940
-# 1534755952314119
-# 4503599627370496
+print(max(solutions, key=solutions.get))
