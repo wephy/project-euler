b104fb16ff4eb70740476ada6d04d017c23d3162
Joseph <xexpozure@gmail.com>
2021-06-10 19:59:48 +0100

    Improve p068 (massively)

--- python/p068.py
+++ python/p068.py
@@ -1,40 +1,39 @@
 # Magic 5-gon ring
 
-import numpy as np
+from itertools import combinations, chain
 
-solutions = set()
-numbers = set(i for i in range(1, 11))
-pairs = set()
+line_sum = (sum(i for i in range(6, 11)) + 2 * sum(i for i in range(1, 6))) / 5
+
+external = [i for i in range(6, 11)]
+internal = [i for i in range(1, 6)]
+pairs = sorted(
+    [*chain.from_iterable((x, x[::-1]) for x in combinations(internal, 2))],
+    reverse=True)
+pair_matches = []
+for ex in external:
+    for pair in pairs:
+        if sum(pair) + ex == line_sum:
+            pair_matches.append([ex, *pair])
+pair_matches_sorted = sorted(pair_matches, reverse=True)
 
 
-def solve(total, sides=np.zeros((5, 3)), used=set(), n=0):
-    if n == 15:
-        if sum(sides[-1]) == total:
-            for pair in sides:
-                pairs.add(tuple(pair))
-            solutions.add("".join([str(int(x)) for x in sides.flatten()]))
-        return
-
-    side, depth = divmod(n, 3)
-    if side == 1:
-        if tuple(sides[0]) in pairs:
-            return
-    if sides[side][depth] != 0:
-        solve(total, sides, used, n+1)
+def solve(solution=[], used=set()):
+    if len(solution) == 5:
+        if solution[-1][2] == solution[0][1]:
+            print(''.join(map(str, [*chain.from_iterable(solution)])))
+            return True
+    elif len(solution) == 0:
+        for match in pair_matches:
+            if solve([match], {match[0]}):
+                break
     else:
-        for number in numbers.difference(used):
-            new_sides = sides.copy()
-            new_sides[side][depth] = number
-            if depth == 2:
-                new_sides[side+1][1] = number
-            if side == 0 and depth == 1:
-                new_sides[-1][2] = number
-            if depth != 2 or sum(new_sides[side]) == total:
-                solve(total, new_sides, used.union(set([number])), n+1)
+        for match in [p for p in pair_matches_sorted if p[0] not in used]:
+            if match[1] == solution[-1][2]:
+                new_solution = solution + [match]
+                new_used = used.copy()
+                new_used.add(match[0])
+                if solve(new_solution, new_used):
+                    return True
 
 
-for total_sum in range(14, 17):
-    solve(total_sum)
-
-solutions = filter(lambda x: len(x) == 16, solutions)
-print(max([*map(int, solutions)]))
+solve()
