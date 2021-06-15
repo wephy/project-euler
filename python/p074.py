4fca38bdd41797321414a421c091f1e116ae15d3
Joseph <xexpozure@gmail.com>
2021-06-15 22:46:11 +0100

    p074

--- python/p074.py
+++ python/p074.py
@@ -7,11 +7,11 @@ FACTORIAL = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880]
 digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
-def chain(n, add_first=True):
+def chain(n, combination=True):
     count = 0
     found = set()
-    if not add_first:
-        n = sum([FACTORIAL[int(i)] for i in str(n)])
+    if not combination:
+        n = sum([FACTORIAL[int(i)] for i in n])
         count += 1
     while n not in found:
         found.add(n)
@@ -23,7 +23,7 @@ def chain(n, add_first=True):
 answer = 0
 for i in range(1, 7):
     for combo in combinations_with_replacement(digits, i):
-        if chain("".join(map(str, combo)), False) == 60:
+        if chain(combo, False) == 60:
             for perm in multiset_permutations(combo):
                 if perm[0] == 0:
                     continue
