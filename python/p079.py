299da1d87f4eb3adcc8f15b94918fe81311d559d
Joseph <xexpozure@gmail.com>
2021-06-18 00:24:04 +0100

    p079 and data file

---
+++ python/p079.py
@@ -0,0 +1,16 @@
+# Passcode derivation
+
+import os
+from itertools import chain
+
+
+with open(os.path.join("..", "data", "p079.txt"), encoding="utf-8") as f:
+    data = f.read().splitlines()
+
+d = {i: set() for i in set(chain.from_iterable(data))}
+
+for attempt in data:
+    d[attempt[0]].update([attempt[1], attempt[2]])
+    d[attempt[1]].update([attempt[2]])
+
+print("".join(sorted(d.keys(), key=lambda x: len(d[x]), reverse=True)))
