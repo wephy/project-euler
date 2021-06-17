4429828be7042c18b987cf4a459e5bc1c66a6bff
Joseph <xexpozure@gmail.com>
2021-06-17 20:00:01 +0100

    p076

---
+++ python/p076.py
@@ -0,0 +1,22 @@
+# Coin sums
+
+from sympy import sieve
+
+primes = list(sieve.primerange(1, 1_000_000))[::-1]
+
+
+def solve(target, integers):
+    count = (target % integers[0] == 0)
+
+    while len(integers) > 1 and target > 0:
+        count += solve(target, integers[1:])
+        target -= integers[0]
+
+    return count
+
+
+answer = 10
+while solve(answer, [p for p in primes if p < answer]) < 5000:
+    answer += 1
+
+print(answer)
