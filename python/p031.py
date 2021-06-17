18499fbb4edd4af93af39a175844705401e3e050
Joseph <xexpozure@gmail.com>
2021-06-17 20:11:35 +0100

    update p031

--- python/p031.py
+++ python/p031.py
@@ -1,12 +1,10 @@
 # Coin sums
 
-def ways(COINS, TARGET, start=0, index=0, count=0):
+def solve(target, coins, start=0, index=0, count=0):
     if index < 7:
-        for i in range(start, TARGET+1, COINS[index]):
-            count = ways(COINS, TARGET, i, index+1, count)
-    else:
-        count += 1
-    return count
+        for i in range(start, target+1, coins[index]):
+            count = solve(target, coins, i, index+1, count)
+    return count + (index >= 7)
 
 
-print(ways([200, 100, 50, 20, 10, 5, 2, 1], 200))
+print(solve(200, [200, 100, 50, 20, 10, 5, 2, 1]))
