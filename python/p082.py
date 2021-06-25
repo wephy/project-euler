9d6c99971eba4a12215c42a596335dacca5c9fb2
Joseph Webb <joseph.webb@dcms.gov.uk>
2021-06-25 14:46:37 +0100

    p082

---
+++ python/p082.py
@@ -0,0 +1,18 @@
+# Path sum: three ways
+
+import os
+import numpy as np
+
+size = 80
+matrix = np.loadtxt(os.path.join("..", "data", "p082.txt"),
+    usecols=range(size), delimiter=",", dtype=int)
+
+for x in range(1, size):
+    column = matrix[:, x] + matrix[:, x-1]
+    for y in range(1, size):
+        column[y] = min(column[y], column[y-1] + matrix[y, x])
+    for y in range(size - 2, -1, -1):
+        column[y] = min(column[y], matrix[y, x] + column[y+1])
+    matrix[:, x] = column
+
+print(min(matrix[:, -1]))
