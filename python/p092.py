354204a4d0014a69b04c4168baa7194c0916b845
wephy <xexpozure@gmail.com>
2021-07-02 10:13:44 +0100

    more concise using unpacking

--- python/p092.py
+++ python/p092.py
@@ -18,7 +18,7 @@ for i in range(568):
 
 F = [1, 1, 2, 6, 24, 120, 720, 5_040, 40_320, 362_880]
 answer = 0
-for combo in combinations_with_replacement([i for i in range(10)], 7):
+for combo in combinations_with_replacement([*range(10)], 7):
     if cache[sum(d**2 for d in combo)]:
         answer += F[7] // np.product([F[v] for v in Counter(combo).values()])
 
