745715fddc7e67341b34f85a5c7d5986a5540d4d
wephy <xexpozure@gmail.com>
2021-07-05 18:38:10 +0100

    improve p091

--- python/p091.py
+++ python/p091.py
@@ -2,18 +2,12 @@
 
 import numpy as np
 
-limit = 50
-
 answer = 0
-for coord in np.ndindex(limit+1, limit+1):
-    perp = (coord / np.gcd(*coord))
-    perp = [perp[1], -perp[0]]
-    new = np.array([coord[0] + perp[0], coord[1] + perp[1]])
-    while new[0] <= 50 and new[1] >= 0:
+for (x, y) in list(np.ndindex(50+1, 50+1))[1:]:
+    slope = [x, y] / np.gcd(x, y)
+    coord = [y, -x] + slope
+    while coord[0] <= 50 and coord[1] <= 0:
+        coord += slope
         answer += 1
-        new[0] += perp[0]
-        new[1] += perp[1]
 
-answer = answer * 2 + limit**2
-
-print(answer)
+print(answer * 2 + 50**2)
