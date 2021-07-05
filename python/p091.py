d773710bbf931f28e760a9563a4e85146919a623
wephy <xexpozure@gmail.com>
2021-07-05 18:39:11 +0100

    improved styling for p091

--- python/p091.py
+++ python/p091.py
@@ -2,12 +2,12 @@
 
 import numpy as np
 
-answer = 0
+count = 0
 for (x, y) in list(np.ndindex(50+1, 50+1))[1:]:
     slope = [x, y] / np.gcd(x, y)
     coord = [y, -x] + slope
     while coord[0] <= 50 and coord[1] <= 0:
         coord += slope
-        answer += 1
+        count += 1
 
-print(answer * 2 + 50**2)
+print((count * 2) + 50**2)
