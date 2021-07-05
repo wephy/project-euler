36f71abb35a41fa1cc35d76081e9b26a4c5323af
wephy <xexpozure@gmail.com>
2021-07-05 18:41:01 +0100

    slight efficiency improvement to p091 using tuple construction opposed to list (faster)

--- python/p091.py
+++ python/p091.py
@@ -4,8 +4,8 @@ import numpy as np
 
 count = 0
 for (x, y) in list(np.ndindex(50+1, 50+1))[1:]:
-    slope = [x, y] / np.gcd(x, y)
-    coord = [y, -x] + slope
+    slope = (x, y) / np.gcd(x, y)
+    coord = (y, -x) + slope
     while coord[0] <= 50 and coord[1] <= 0:
         coord += slope
         count += 1
