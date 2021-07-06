e7d9219224ec6bfe126667d0a363786bb4b89001
Joseph <xexpozure@gmail.com>
2021-07-06 20:23:47 +0100

    minor changes to p096

--- python/p096.py
+++ python/p096.py
@@ -16,16 +16,16 @@ def solve(board):
     if 0 not in chain.from_iterable(board):
         return int("".join([str(i) for i in board[0, 0:3]]))
     candidates = {(y, x): digits - set([
-        *board[y, :], *board[:, x],
-        *chain.from_iterable(board[y//3*3:y//3*3+3, x//3*3:x//3*3+3])]
-        ) for (y, x) in coords if board[y, x] == 0}
+        *board[y, :], *board[:, x], *chain.from_iterable(
+            board[y//3*3:y//3*3+3, x//3*3:x//3*3+3])
+        ]) for (y, x) in coords if board[y, x] == 0}
     coord = min(candidates, key=lambda x: len(candidates[x]))
     for i in candidates[coord]:
         board[coord] = i
         number = solve(board)
         if number:
             return number
-        board[coord] = 0
+    board[coord] = 0
 
 
 print(sum(solve(sudokus[n]) for n in range(50)))
