6395e5b2295f3131fcb9fd47c2102eb5073b83e8
Joseph <xexpozure@gmail.com>
2021-07-06 15:24:24 +0100

    improved pep8 styling

--- python/p089.py
+++ python/p089.py
@@ -12,9 +12,9 @@ numerals = {"M": 1000, "CM": 900, "D": 500, "CD": 400, "C": 100, "XC": 90,
 def reduce(number):
     result = numerals[number[-1]]
     for i in range(len(number) - 1):
-        result += (numerals[number[i]] *
-            (-1) ** (numerals[number[i]] < numerals[number[i+1]]))
-    
+        result += (numerals[number[i]] * (-1) ** (
+                   numerals[number[i]] < numerals[number[i+1]]))
+
     minimal = ""
     while result > 0:
         for r, i in numerals.items():
