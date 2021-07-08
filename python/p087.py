cef7680f853398527e01fa3d39567af473ca9355
Joseph <xexpozure@gmail.com>
2021-07-08 17:29:12 +0100

    updated styling

--- python/p087.py
+++ python/p087.py
@@ -6,7 +6,7 @@ from bisect import bisect_left
 limit = 50_000_000
 primes = list(sieve.primerange(1, limit**0.5))
 powers = [[p**i for p in primes[:bisect_left(primes, limit**(1 / i))]]
-    for i in range(2, 5)]
+          for i in range(2, 5)]
 
 found = set()
 for a in powers[0]:
@@ -15,4 +15,4 @@ for a in powers[0]:
         for c in powers[2][:bisect_left(powers[2], remainder)]:
             found.add(remainder - c)
 
-print(len(found))            
+print(len(found))
