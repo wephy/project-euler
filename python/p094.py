7968741a99d2e8c2d8648a3616a816bbde9909d3
Joseph <xexpozure@gmail.com>
2021-07-08 17:10:35 +0100

    improve p094

--- python/p094.py
+++ python/p094.py
@@ -1,20 +1,17 @@
 # Almost equilateral triangles
 
-answer = 0
-v0, v1 = 0, 1
-u0, u1 = 1, 2
-while True:
-    p = 2 * (u1**2 + v1**2) + 2 * (u1**2 - v1**2)
-    if p > 1_000_000_000:
-        break
-    answer += p
+def solve(limit):
+    answer = 0
+    v0, v1 = 0, 1
+    u0, u1 = 1, 2
+    while True:
+        v0, v1 = v1, 4 * v1 - v0
+        u0, u1 = u1, 4 * u1 - u0
+        for p in [2 * (u0**2 + v0**2) + 2 * (u0**2 - v0**2),
+                  2 * (v0**2 + v1**2) + (4 * v1 * v0)]:
+            if p > limit:
+                return answer
+            answer += p
 
-    v0, v1 = v1, 4 * v1 - v0
-    u0, u1 = u1, 4 * u1 - u0
 
-    p = 2 * (v0**2 + v1**2) + (4 * v1 * v0)
-    if p > 1_000_000_000:
-        break
-    answer += p
-
-print(answer)
+print(solve(1_000_000_000))
