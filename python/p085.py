64d700536657ad70d18145ac2b2b864b7f48e0ca
Joseph <xexpozure@gmail.com>
2021-07-08 12:24:03 +0100

    pep8 styling

--- python/p085.py
+++ python/p085.py
@@ -1,6 +1,8 @@
 # Counting rectangles
 
-def h(w): return ((1 + 16 * 2_000_000 / (w * (w + 1)))**0.5 - 1) / 2
+def h(w):
+    return ((1 + 16 * 2_000_000 / (w * (w + 1)))**0.5 - 1) / 2
+
 
 width = min(range(1, 54), key=lambda w: h(w) % 1)
 
