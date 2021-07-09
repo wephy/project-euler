b7db2f5d824825cf83f027fa2767afee4c3fad29
Joseph <xexpozure@gmail.com>
2021-07-09 19:22:33 +0100

    update p098

---
+++ python/p093.py
@@ -0,0 +1,7 @@
+# Arithmetic expressions
+
+from itertools import combinations_with_replacement, permutations, product
+
+
+def count(digits):
+    
--- python/p098.py
+++ python/p098.py
@@ -2,36 +2,30 @@
 
 import os
 import numpy as np
+from itertools import combinations
 
 data = set(np.char.strip(np.loadtxt(os.path.join("..", "data", "p098.txt"),
                                     delimiter=",", dtype=str), '"'))
 
-sorted_words = {word: sorted(word) for word in data}
+words = {i: [w for w in data if len(w) == i] for i in range(15)}
+w_hashes = {w: hash(tuple(sorted(w))) for w in data}
+anagrams = np.array([(x, y) for lengths in words.values() for (x, y) in
+                     combinations(lengths, 2) if w_hashes[x] == w_hashes[y]
+                     and x != y[::-1]])
 
-anagrams = []
-for word in data.copy():
-    data.remove(word)
-    for pair in data:
-        if sorted_words[word] == sorted_words[pair] and pair != word[::-1]:
-            anagrams.append((word, pair))
-
-squares = set([i**2 for i in range(
-    int(10**(max(len(x[0]) for x in anagrams) + 1)**0.5))])
-squares_placements = {square: sorted(
-        {x: tuple(i for i, c in enumerate(str(square)) if c == x)
-         for x in set(str(square))}.values()) for square in squares}
+squares = set([i**2 for i in range(int(
+               10**len(max(anagrams[:, 0], key=len))**0.5))])
+s_hashes = {sq: hash(tuple(sorted(tuple(i for i, c in enumerate(str(sq))
+            if c == y) for y in set(str(sq))))) for sq in squares}
 
 solutions = []
-for word, pair in anagrams:
-    word_placements = {x: tuple(i for i, c in enumerate(word) if c == x)
-                       for x in set(word)}
-    for square in squares:
-        if sorted(word_placements.values()) == squares_placements[square]:
-            substitutions = {x: str(square)[word_placements[x][0]]
-                             for x in word_placements.keys()}
+for w, pair in anagrams:
+    struct = {x: tuple(i for i, c in enumerate(w) if c == x) for x in set(w)}
+    for sq in squares:
+        if hash(tuple(sorted(struct.values()))) == s_hashes[sq]:
+            substitutions = {x: str(sq)[struct[x][0]] for x in struct.keys()}
             number = "".join([substitutions[x] for x in pair])
             if number[0] != "0" and int(number) in squares:
-                solutions.append(square)
-                solutions.append(int(number))
+                solutions += [sq, int(number)]
 
 print(max(solutions))
